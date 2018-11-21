162972ea922059e6bcf972251c8ef386efc4150c
fdlessard <fdlessard@gmail.com>
2018-11-21 16:27:53 -0500

    Fix default profile

---
+++ src/main/java/io/fdlessard/codebites/magiceightball/postgresql/configurations/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package io.fdlessard.codebites.magiceightball.postgresql.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+}
